c2857869efb7f7e871b9cde52be8963d0f7b6bf3
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-09-26 00:06:22 +0330

    import: user schema & repository module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserSchema } from './db/user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{
+      name: User.name, schema: UserSchema
+    }])],
   providers: [UsersService],
   controllers: [UsersController]
 })
-export class UsersModule {}
+export class UsersModule { }
