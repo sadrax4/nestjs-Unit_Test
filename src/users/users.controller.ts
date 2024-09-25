576b0990f1d0a80d6243143ce9f53df8bea4f0bb
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-09-25 23:53:34 +0330

    build: user schema

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Get(':userId')
+    async getUser(@Param('userId') userId: string): Promise<User> {
+      return this.usersService.getUserById(userId);
+    }
+  
+}
