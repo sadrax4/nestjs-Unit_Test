d0423a180cca9736cf7e5b4ef95db81c37346694
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-09-25 23:54:58 +0330

    build: create user controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,19 +1,26 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from './db/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService) { }
 
     @Get(':userId')
     async getUser(@Param('userId') userId: string): Promise<User> {
-      return this.usersService.getUserById(userId);
+        return this.usersService.getUserById(userId);
     }
-    
+
     @Get()
     async getUsers(): Promise<User[]> {
         return this.usersService.getUsers();
     }
-  
+
+    @Post()
+    async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
+        return this.usersService.createUser(createUserDto.email, createUserDto.age)
+    }
+
+
 }
