265cd620f42f2751330f31b727f92e4c976f2f4e
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-09-25 23:48:52 +0330

    import: user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { DatabaseModule } from '@app/common';
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
-    DatabaseModule
+    DatabaseModule,
+    UsersModule
   ],
   controllers: [],
   providers: [],
