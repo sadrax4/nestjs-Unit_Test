9d45da4b023d7f6d91b93df9b85e43ea5f3f5964
sadra soleimani <sadrasadrasadra6@gmail.com>
2024-09-26 11:25:49 +0330

    test: update user controller

--- src/users/test/user.controller.spec.ts
+++ src/users/test/user.controller.spec.ts
@@ -44,42 +44,65 @@ describe('UsersController', () => {
 
     describe('getUsers', () => {
         describe('when getUsers is called', () => {
-          let users: User[];
-    
-          beforeEach(async () => {
-            users = await usersController.getUsers();
-          })
-    
-          test('then it should call usersService', () => {
-            expect(usersService.getUsers).toHaveBeenCalled();
-          })
-    
-          test('then it should return users', () => {
-            expect(users).toEqual([userStub()])
-          })
-        })
-      })
+            let users: User[];
 
-      describe('createUser', () => {
-        describe('when createUser is called', () => {
-          let user: User;
-          let createUserDto: CreateUserDto
-    
-          beforeEach(async () => {
-            createUserDto = {
-              email: userStub().email,
-              age: userStub().age
-            }
-            user = await usersController.createUser(createUserDto);
-          })
-    
-          test('then it should call usersService', () => {
-            expect(usersService.createUser).toHaveBeenCalledWith(createUserDto.email, createUserDto.age);
-          })
-    
-          test('then it should return a user', () => {
-            expect(user).toEqual(userStub())
-          })
+            beforeEach(async () => {
+                users = await usersController.getUsers();
+            })
+
+            test('then it should call usersService', () => {
+                expect(usersService.getUsers).toHaveBeenCalled();
+            })
+
+            test('then it should return users', () => {
+                expect(users).toEqual([userStub()])
+            })
         })
-      })
+    })
+
+    describe('createUser', () => {
+        describe('when createUser is called', () => {
+            let user: User;
+            let createUserDto: CreateUserDto
+
+            beforeEach(async () => {
+                createUserDto = {
+                    email: userStub().email,
+                    age: userStub().age
+                }
+                user = await usersController.createUser(createUserDto);
+            })
+
+            test('then it should call usersService', () => {
+                expect(usersService.createUser).toHaveBeenCalledWith(createUserDto.email, createUserDto.age);
+            })
+
+            test('then it should return a user', () => {
+                expect(user).toEqual(userStub())
+            })
+        })
+    })
+
+    describe('updateUser', () => {
+        describe('when updateUser is called', () => {
+            let user: User;
+            let updateUserDto: UpdateUserDto;
+
+            beforeEach(async () => {
+                updateUserDto = {
+                    age: 98,
+                    favoriteFoods: ['pizza']
+                }
+                user = await usersController.updateUser(userStub().userId, updateUserDto);
+            })
+
+            test('then it should call usersService', () => {
+                expect(usersService.updateUser).toHaveBeenCalledWith(userStub().userId, updateUserDto);
+            })
+
+            test('then it should return a user', () => {
+                expect(user).toEqual(userStub())
+            })
+        })
+    })
 })
